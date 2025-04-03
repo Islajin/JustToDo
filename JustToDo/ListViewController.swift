725dd3cd10499c661f58ab9b073ad75396cfd8ae
Isla <Islajiin@gmail.com>
2025-04-03 17:38:13 +0900

    [feat]- prepare
    listVC는 게시물 리스트가 있는 테이블 뷰를 의미

--- JustToDo/ListViewController.swift
+++ JustToDo/ListViewController.swift
@@ -10,8 +10,17 @@ import UIKit
 class ListViewController: UIViewController {
 
     
+    @IBOutlet weak var toDoTableView: UITableView!
     var toDoList = [String]()
     
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // UIStoryboardSegue 실행중인 segue 객체, sender : Any
+        //sender은 호출한 객체 , 다양한 뷰가 sender가 될 수 있음, sender가 없는 경우도 있음
+        if let vc = segue.destination.children.first as? AddViewController {
+         vc.listVC = self
+            //as는 타입캐스팅 연산자
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
