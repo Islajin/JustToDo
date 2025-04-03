147810cf4d73fd4ad37770cff93cc97a661ad298
Isla <Islajiin@gmail.com>
2025-04-03 16:18:02 +0900

    [feat] - UITableViewDataSource 추가해서 toDoList에 표시
    문자 배열로 만든 todolist를 UITableViewDataSource 이용해서 화면에 표시

--- JustToDo/ViewController.swift
+++ JustToDo/ViewController.swift
@@ -9,11 +9,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    var toDoList = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+}
+
+
+
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return toDoList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        cell.textLabel?.text=toDoList[indexPath.row]
+        return cell
+    }
+    
+    
 }
 
