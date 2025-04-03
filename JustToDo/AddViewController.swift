cb3a6276958ecb0fcf06b1d4018fbfd688872d0f
Isla <Islajiin@gmail.com>
2025-04-03 17:11:26 +0900

    [feat]- cancel이랑 save버튼 연결
    dismiss 를 cancel과 save에서 호출함

--- JustToDo/AddViewController.swift
+++ JustToDo/AddViewController.swift
@@ -9,7 +9,31 @@ import UIKit
 
 class AddViewController: UIViewController {
 
-    override func viewDidLoad() {
+    
+    var listVC: ListViewController?
+    
+    @IBOutlet weak var inputField: UITextField!
+    
+    @IBAction func cancel(_ sender: Any) {
+        dismiss(animated: true)
+    }
+    
+
+    @IBAction func save(_ sender: Any) {
+        guard let text = inputField.text else {
+            
+            let alert = UIAlertController(title: "알림", message: "메시지를 입력해 주세요.", preferredStyle: .alert)
+            let okAction = UIAlertAction(title: "확인", style: .default)
+            alert.addAction(okAction)
+            present(alert, animated: true)
+            return
+                    }
+        
+        dismiss(animated: true)
+    }
+    
+    
+      override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
