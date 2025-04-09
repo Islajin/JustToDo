63873122998800fca5ea5ba0fa7b2acff4c7b1cb
Isla <Islajiin@gmail.com>
2025-04-09 12:46:38 +0900

    [feat]-옵셔널 체이닝

--- JustToDo/MyPlayground.playground/Contents.swift
+++ JustToDo/MyPlayground.playground/Contents.swift
@@ -1,3 +1,21 @@
 import UIKit
 
 var greeting = "Hello, playground"
+
+greeting.count.description
+//체이닝 Chaining
+
+//옵셔널 체이닝
+
+var optionalGreeting: String? = "Hello, playground"
+
+let a = optionalGreeting!.count //XXX사용 지양
+let b = optionalGreeting?.count
+//nil을 강제추출하면 크래쉬가 발생함
+//? 는 옵셔널 체이닝, !는 강제 추출 바인딩
+// 옵셔널 표현식일때만 느낌표나 물음표를 붙이면 됨
+
+
+
+
+
